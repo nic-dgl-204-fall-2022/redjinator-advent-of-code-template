b6e4a258b1780cda6bdd1459e80cf7cf1751c8db
Redjinator <rmcpherson@northislandcollege.ca>
2022-10-16 23:23:50 -0700

    Complete: day 2 part 1 & 2 - not refactored

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,29 +1,58 @@
 fun main() {
 
+    fun remove(arr: IntArray, index: Int): IntArray {
+        if (index < 0 || index >= arr.size) {
+            return arr
+        }
+
+        val result = arr.toMutableList()
+        result.removeAt(index)
+        return result.toIntArray()
+    }
+
+    fun getSurfaceArea(l: Int, w: Int, h: Int): Int {
+        val surfaceArea = 2 * (l * w) + 2 * (w * h) + 2 * (h * l)
+        return (surfaceArea + minOf(l * w, w * h, h * l))
+    }
+
+    fun getSmallestPerimeter(l: Int, w: Int, h: Int): Int {
+        val maxUnit = maxOf(l, w, h)
+        val arr = intArrayOf(l, w, h)
+        val index = arr.indexOf(maxUnit)
+        val arr2 = remove(arr, index)
+        return arr2[0] + arr2[0] + arr2[1] + arr2[1]
+    }
+
+    fun getVolumeCubed(l: Int, w: Int, h: Int): Int {
+        return l*w*h
+    }
+
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputSplit = input.map { it.split('x') }
+        var totalPaper: Int = 0
+
+        for (i in inputSplit.indices) {
+            totalPaper += getSurfaceArea(inputSplit[i][0].toInt(), inputSplit[i][1].toInt(), inputSplit[i][2].toInt())
+        }
+        return totalPaper
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputSplit = input.map { it.split('x')}
+
+        var peremiter: Int = 0
+        var volume: Int = 0
+
+        for (i in inputSplit.indices) {
+            peremiter += getSmallestPerimeter(inputSplit[i][0].toInt(), inputSplit[i][1].toInt(), inputSplit[i][2].toInt())
+            volume    += getVolumeCubed(inputSplit[i][0].toInt(), inputSplit[i][1].toInt(), inputSplit[i][2].toInt())
+        }
+        return peremiter + volume
     }
 
-    var totalPaper : Double = 0.0
-
-    fun getSurfaceArea(l: Double, w: Double, h: Double): Double {
-        val surfaceArea = 2*(l*w) + 2*(w*h) + 2*(h*l)
-        val smallestSide = minOf(l*w, w*h, h*l)
-        return (surfaceArea + smallestSide)
-    }
-
-    val inputFile = readInput("inputdata").map { it.split('x') }
-
-    for (i in inputFile.indices) {
-        val l = inputFile[i][0].toDouble()
-        val w = inputFile[i][1].toDouble()
-        val h = inputFile[i][2].toDouble()
-        totalPaper += getSurfaceArea(l, w, h)
-    }
-
-    println("Total Paper Req: $totalPaper")
+    val input = readInput("inputdata")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
