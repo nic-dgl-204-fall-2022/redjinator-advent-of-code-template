be3872ace261cd529cae4efe25d7cee69d55b92a
Redjinator <rmcpherson@northislandcollege.ca>
2022-10-16 18:55:07 -0700

    Update Day02.kt

--- src/Day02.kt
+++ src/Day02.kt
@@ -10,28 +10,18 @@ fun main() {
 
     var totalPaper : Double = 0.0
 
-    fun getSmallestSide(l: Double, w: Double, h: Double): Double {
-        return if (l*w <= w*h && l*w <= h*l) {
-            l*w
-        } else if (w*h <= l*w && w*h <= h*l) {
-            w*h
-        } else {
-            h*l
-        }
-    }
-
     fun getSurfaceArea(l: Double, w: Double, h: Double): Double {
         val surfaceArea = 2*(l*w) + 2*(w*h) + 2*(h*l)
-        val extra = getSmallestSide(l, w, h)
-        return (surfaceArea + extra)
+        val smallestSide = minOf(l*w, w*h, h*l)
+        return (surfaceArea + smallestSide)
     }
 
-    val inputSplit = readInput("inputdata").map { it.split('x') }
+    val inputFile = readInput("inputdata").map { it.split('x') }
 
-    for (i in inputSplit.indices) {
-        val l = inputSplit[i][0].toDouble()
-        val w = inputSplit[i][1].toDouble()
-        val h = inputSplit[i][2].toDouble()
+    for (i in inputFile.indices) {
+        val l = inputFile[i][0].toDouble()
+        val w = inputFile[i][1].toDouble()
+        val h = inputFile[i][2].toDouble()
         totalPaper += getSurfaceArea(l, w, h)
     }
 
