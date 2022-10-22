f95d6fa124ad39e0fe8b715d23cdf754be8e001f
ash-teach <ashley.blacquiere@nic.bc.ca>
2022-10-21 23:46:22 -0700

    feedback

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,15 +1,6 @@
+import java.lang.Integer.min
+
 fun main() {
-    
-    fun remove(arr: IntArray, index: Int): IntArray {
-        if (index < 0 || index >= arr.size) {
-            return arr
-        }
-
-        val result = arr.toMutableList()
-        result.removeAt(index)
-        return result.toIntArray()
-    }
-
 
     fun getSurfaceArea(l: Int, w: Int, h: Int): Int  = 2 * (l * w) + 2 * (w * h) + 2 * (h * l)
 
@@ -18,15 +9,10 @@ fun main() {
 
     // Part 1
     fun part1(input: List<String>): Int {
+        var totalPaper = 0
 
-        val measurements = input
-            .map { it.split('x') }
-            .map { it.map { it.toInt() }}
-
-        var totalPaper   = 0
-
-        for (i in measurements.indices) {
-            val (length, width, height) = Triple(measurements[i][0], measurements[i][1], measurements[i][2])
+        for (dimensions in input) {
+            val (length, width, height) = dimensions.split("x").map { it.toInt() }
             val paperRequired = getSurfaceArea(length, width, height)
             val slackRequired = getExtraSlack(length, width, height)
 
@@ -35,13 +21,7 @@ fun main() {
         return totalPaper
     }
 
-    fun getSmallestPerimeter(l: Int, w: Int, h: Int): Int {
-        val measurementSet        = intArrayOf(l, w, h)
-        val indexOfMax            = measurementSet.indexOf(maxOf(l, w, h))
-        val measurementPair       = remove(measurementSet, indexOfMax)
-
-        return measurementPair[0] + measurementPair[0] + measurementPair[1] + measurementPair[1]
-    }
+    fun getSmallestPerimeter(l: Int, w: Int, h: Int): Int = 2 * minOf(l + w, w + h, l + h)
 
     fun getVolumeCubed(l: Int, w: Int, h: Int): Int { return l*w*h }
 
